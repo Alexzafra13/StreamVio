37c7f767728f0db95e105141909b5fb1ccfad587
Alejandro <ale_zafra_13@hotmail.com>
2025-03-31 11:27:44 +0200

    23

--- clients/web/astro.config.mjs
+++ clients/web/astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
+
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind()
-  ],
+  integrations: [react(), tailwind()],
   vite: {
     ssr: {
       noExternal: ["react-icons"],
